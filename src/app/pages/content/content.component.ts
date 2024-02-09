eae52227552623d2c0bcc75e32b36a6d9527f2bf
FSLucas22 <lucassantos@presconinformatica.com.br>
2024-02-09 20:53:57 -0300

    feat: Create content page

--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-content',
@@ -6,7 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent implements OnInit {
-
+  @Input() photo: string = "https://via.placeholder.com/900x400";
+  @Input() photoAlt: string = "Content photo";
+  @Input() title: string = "Content title";
+  @Input() body: string = "Content body";
   constructor() { }
 
   ngOnInit(): void {
