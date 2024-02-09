b60923c78d6fca4a1570e36083bfd49697b1d04f
FSLucas22 <lucassantos@presconinformatica.com.br>
2024-02-08 23:42:30 -0300

    feat: Add customization to big-card component

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./big-card.component.css']
 })
 export class BigCardComponent implements OnInit {
+  @Input() photo: string = "";
+  @Input() photoAlt: string = "Main card image"
+  @Input() title: string = "";
+  @Input() cardLink: string = "#";
+  @Input() description: string = "";
 
   constructor() { }
 
